69769a4a66a91c6e2ecdf0d6ae0aa11e4b85fa7a
19906126897 <2063980370@qq.com>
2023-06-27 10:05:25 +0800

    first

---
+++ src/main/java/com/icedragongame/IceDragonGameApplication.java
@@ -0,0 +1,11 @@
+package com.icedragongame;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IceDragonGameApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(IceDragonGameApplication.class,args);
+    }
+}
