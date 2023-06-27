58db01180168daa2b197d92ace7045dd82daa3f5
19906126897 <2063980370@qq.com>
2023-06-27 11:31:24 +0800

    类的规范

--- src/main/java/com/icedragongame/entity/Reply.java
+++ src/main/java/com/icedragongame/entity/Reply.java
@@ -1,50 +1,17 @@
 package com.icedragongame.entity;
 
-import lombok.Builder;
 import lombok.Data;
 
 /**
  * @ClassName : Reply  //类名
  * @Description : reply class  //描述
- * @Author : wenrui //作者
+ * @Author : wenrui //作者mydb
  * @Date: 2023/6/27  10:12
  */
 
 @Data
-@Builder
 public class Reply {
-    private int reply_id;
-    private String reply_context;
-    private String reply_time;
-
-    public Reply(int reply_id, String reply_context, String reply_time) {
-        this.reply_id = reply_id;
-        this.reply_context = reply_context;
-        this.reply_time = reply_time;
-    }
-    public int getReply_id() {
-        return reply_id;
-    }
-
-    public void setReply_id(int reply_id) {
-        this.reply_id = reply_id;
-    }
-
-    public String getReply_context() {
-        return reply_context;
-    }
-
-    public void setReply_context(String reply_context) {
-        this.reply_context = reply_context;
-    }
-
-    public String getReply_time() {
-        return reply_time;
-    }
-
-    public void setReply_time(String reply_time) {
-        this.reply_time = reply_time;
-    }
-
-
+    private int replyId;
+    private String replyContext;
+    private String replyTime;
 }
