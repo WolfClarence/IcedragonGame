697a8a740ef09db592dc9621ac4c7c44e4206323
ppy <1017369209@qq.com>
2023-06-27 11:35:04 +0800

    test

--- src/main/java/com/icedragongame/IceDragonGameApplication.java
+++ src/main/java/com/icedragongame/IceDragonGameApplication.java
@@ -3,6 +3,7 @@ package com.icedragongame;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 //woshigeshabi
+//test
 @SpringBootApplication
 public class IceDragonGameApplication {
     public static void main(String[] args) {
