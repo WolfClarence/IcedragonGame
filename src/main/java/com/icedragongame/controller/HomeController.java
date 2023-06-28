fe7951c6f2e690727dea418b8587dfe2fe330b1e
ppy <1017369209@qq.com>
2023-06-28 10:50:25 +0800

    新游戏 热度游戏 关注游戏

--- src/main/java/com/icedragongame/controller/HomeController.java
+++ src/main/java/com/icedragongame/controller/HomeController.java
@@ -1,10 +1,11 @@
 package com.icedragongame.controller;
 
+import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
 import com.icedragongame.common.R;
 import com.icedragongame.entity.Post;
 import com.icedragongame.service.PostService;
-import com.icedragongame.service.ReplyService;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -18,16 +19,29 @@ public class HomeController {
     @Resource
     private PostService postService;
 
-    @Resource
-    private ReplyService replyService;
-
     @GetMapping("/newGame")
-    public R<List<Post>> getNewGame(){
-
+    public R<List<Post>> newGame(){
+        QueryWrapper<Post> query = new QueryWrapper<>();
+        query.orderByDesc("build_time");
+        query.last("limit 10");
+        List<Post> list = postService.list(query);
+        return R.success(list);
     }
 
     @GetMapping("/hotgame")
-    public R<List<Post>> getHotgame(){
+    public R<List<Post>> hotgame() {
+        QueryWrapper<Post> query = new QueryWrapper<>();
+        query.orderByDesc("reply_num", "scan_num");
+        query.last("limit 10");
+        List<Post> list = postService.list(query);
+        return R.success(list);
+    }
 
+    @GetMapping("/followedGame/{username}")
+    public R<List<Post>> followedGame(@PathVariable(value = "username") String username) {
+        QueryWrapper<Post> query = new QueryWrapper<>();
+        query.eq("username",username);
+        List<Post> list = postService.list(query);
+        return R.success(list);
     }
 }
