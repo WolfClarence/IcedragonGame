ec2800008096d94a38210e2995a72ee59f7d57b6
gengxuelong <3349495429@qq.com>
2023-06-30 12:12:24 +0800

    修改现有service的updateById(entity)和update(entity,wrapper)两个方法的默认操作,添加了先根据id查询到数据库的entityFromBase,再根据传入的可能含空值的entityRaw类进行数据更新,最后吧数据更新过的entityFromBase传入update的参数中

--- src/main/java/com/icedragongame/service/PostService.java
+++ src/main/java/com/icedragongame/service/PostService.java
@@ -1,8 +1,24 @@
 package com.icedragongame.service;
 
+import com.baomidou.mybatisplus.core.conditions.Wrapper;
 import com.baomidou.mybatisplus.extension.service.IService;
 import com.icedragongame.entity.Post;
+import com.icedragongame.utils.MyBeanUtils;
 
 public interface PostService extends IService<Post> {
     //Post selectPostById(Serializable postId);
+
+    @Override
+    default boolean updateById(Post post) {
+        Post entity = getById(post.getPostId());
+        post  = MyBeanUtils.updateBeanBySource(post,entity);
+        return IService.super.updateById(post);
+    }
+
+    @Override
+    default boolean update(Post entity, Wrapper<Post> updateWrapper) {
+        Post entityFromBase = getById(entity.getPostId());
+        entity  = MyBeanUtils.updateBeanBySource(entity,entityFromBase);
+        return IService.super.update(entity, updateWrapper);
+    }
 }
