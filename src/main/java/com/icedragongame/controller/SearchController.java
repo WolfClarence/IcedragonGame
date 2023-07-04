7e7f869653a6a78b001cc2a42ad7fe5d06fb109d
gengxuelong <3349495429@qq.com>
2023-07-04 23:04:03 +0800

    新增跨源设置

--- src/main/java/com/icedragongame/controller/SearchController.java
+++ src/main/java/com/icedragongame/controller/SearchController.java
@@ -15,8 +15,6 @@ import io.swagger.annotations.ApiOperation;
 import org.springframework.web.bind.annotation.*;
 
 import javax.annotation.Resource;
-import java.util.ArrayList;
-import java.util.List;
 
 /**
  * <p>
