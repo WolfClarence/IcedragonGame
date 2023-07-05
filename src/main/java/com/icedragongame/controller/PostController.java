5bea5517b4106b67c42dd87ebcbde34264a99802
wzy <3238366763@qq.com>
2023-07-05 17:20:45 +0800

    修改/postingPage/{num}

--- src/main/java/com/icedragongame/controller/PostController.java
+++ src/main/java/com/icedragongame/controller/PostController.java
@@ -20,7 +20,10 @@ import com.icedragongame.vo.ReplyVo;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 import javax.annotation.Resource;
 import java.text.SimpleDateFormat;
@@ -74,7 +77,7 @@ public class PostController {
      */
     @Resource
     private ReplyService replyService;
-
+    @Resource
     private CategoryService categoryService;
 
     /**
@@ -102,12 +105,12 @@ public class PostController {
         LambdaQueryWrapper<Post> queryWapper = new LambdaQueryWrapper<>();
         //获取游戏id
 
-        if(num!=null&&num.isEmpty()){
+        if(!(num==null||num.isEmpty())){
             Category category = categoryService.getOne( new LambdaUpdateWrapper<Category>().eq(Category::getCategoryName,num));
             if(category==null){
                 return R.error(SystemError.NO_THIS_CATEGORY);
             }
-            categoryId = categoryService.getOne(new LambdaUpdateWrapper<Category>().eq(Category::getCategoryName,category)).getId();
+            categoryId = category.getId();
             posts = postService.list(queryWapper.eq(Post::getCategoryId,categoryId));
         }
 
